03b9bd1fb5103b2a5525afaed1065f358c036b24
Commander R <wmxavier2004@gmail.com>
2022-12-12 12:49:12 +0100

    e

--- backend.js
+++ backend.js
@@ -14,6 +14,18 @@ app.get('/developers', async (req, res) => {
     res.render(__dirname + '/pages/developers', {  });
 });
 
+app.get("/mauro", (req, res) => {
+    res.redirect("https://discord.com/users/271285474516140033");
+})
+
+app.get("/jasper", (req, res) => {
+    res.redirect("https://discord.com/users/710853310361370624");
+})
+
+app.get("/martin", (req, res) => {
+    res.redirect("https://discord.com/users/459252483902930955");
+})
+
 app.get('/discord', (req, res) => {
     res.redirect("https://discord.gg/cs5Jukrnt7");
 });
